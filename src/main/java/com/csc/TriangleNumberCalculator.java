be56ea31c85a8a3aab4a400d4f8496bff0eff22d
Owen Ceresa <co84635@mail.harpercollege.edu>
2024-10-20 20:55:54 -0500

    completed lab

--- src/main/java/com/csc/TriangleNumberCalculator.java
+++ src/main/java/com/csc/TriangleNumberCalculator.java
@@ -1,5 +1,21 @@
 package com.csc;
 
 public class TriangleNumberCalculator {
-  // Your code goes here!
+    public static int value(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException();
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return n + value(n - 1);
+        }
+    }
+
+    public static int add(int n, int m) {
+        return value(n) + value(m);
+    }
+
+    public static int subtract(int n, int m) {
+        return value(n) - value(m);
+    }
 }
--- src/test/java/com/csc/TestTriangleNumberCalculator.java
+++ src/test/java/com/csc/TestTriangleNumberCalculator.java
@@ -1,15 +1,63 @@
 package com.csc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TestTriangleNumberCalculator {
 
-  TriangleNumberCalculator calculator;
+    TriangleNumberCalculator calculator;
 
-  @BeforeEach
-  void setUp() {
-    calculator = new TriangleNumberCalculator();
-  }
+    @BeforeEach
+    void setUp() {
+        calculator = new TriangleNumberCalculator();
+    }
 
-  // Your tests go here
+    @Test
+    void itWorks() {
+        assertEquals(true, true);
+    }
+    
+    @Test
+    public void testValueZero() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            TriangleNumberCalculator.value(0);
+        });
+    }
+
+    @Test
+    public void testValueNegative() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            TriangleNumberCalculator.value(-1);
+        });
+    }
+
+    @Test
+    public void testValueOne() {
+        assertEquals(1, TriangleNumberCalculator.value(1));
+    }
+
+    @Test
+    public void testValueSeven() {
+        assertEquals(28, TriangleNumberCalculator.value(7));
+    }
+
+    @Test
+    public void testAddTriangleNumbers() {
+        assertEquals(56, TriangleNumberCalculator.add(7, 7));
+        assertEquals(42, TriangleNumberCalculator.add(8, 3));
+    }
+
+    @Test
+    public void testSubtractTriangleNumbers() {
+        assertEquals(35, TriangleNumberCalculator.subtract(9, 4));
+        assertEquals(30, TriangleNumberCalculator.subtract(8, 3));
+    }
+
+    @Test
+    public void testBasicScenario() {
+        assertEquals(3, TriangleNumberCalculator.value(2));
+    }
 }
